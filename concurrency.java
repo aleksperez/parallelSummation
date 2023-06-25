2dd3502458b874a9ac7174060b1faf02f38992a6
Alex Perez <alieperez96@gmail.com>
2023-06-25 12:01:17 -0700

    added comments describing code + formatted

--- concurrency.java
+++ concurrency.java
@@ -1,11 +1,9 @@
 import java.util.Random;
 
 public class concurrency {
-    public static int[] array = new int[20000*10000];
+    public static int[] array = new int[20000 * 10000];
 
-
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Random rand = new Random();
 
         for (int i = 0; i < array.length; i++) {
@@ -13,14 +11,14 @@ public class concurrency {
         }
 
         long start = System.currentTimeMillis();
-        System.out.println("\nSingle thread summation total: "+thread.getSum(array));
-        System.out.println("Elapsed time: " + (System.currentTimeMillis() - start+"milliseconds"));
+        System.out.println("\nSingle thread summation total: " + thread.getSum(array));
+        System.out.println("Elapsed time: " + (System.currentTimeMillis() - start + " milliseconds"));
 
         System.out.println("--------------");
 
         start = System.currentTimeMillis();
-        System.out.println("Parallel thread summation total: "+thread.getParallelSum(array));
-        System.out.println("Elapsed time: " + (System.currentTimeMillis() - start+"milliseconds"));
+        System.out.println("Parallel thread summation total: " + thread.getParallelSum(array));
+        System.out.println("Elapsed time: " + (System.currentTimeMillis() - start + " milliseconds"));
     }
 }
 
--- thread.java
+++ thread.java
@@ -5,54 +5,59 @@ public class thread extends Thread {
     private int high;
     private int partial;
 
-    public thread(int[] arr, int low, int high){
+    public thread(int[] arr, int low, int high) {
         this.arr = arr;
         this.low = low;
         this.high = Math.min(high, arr.length);
 
     }
-    public void run(){
+
+    public void run() {
         partial = calculateSum(arr, low, high);
     }
-    public int getPartialSum(){
+
+    public int getPartialSum() {
         return partial;
     }
-    public static int getSum(int[] arr){
+
+    public static int getSum(int[] arr) {
         return calculateSum(arr, 0, arr.length);
     }
-    public static int calculateSum(int[] arr, int low, int high){
+
+    public static int calculateSum(int[] arr, int low, int high) {
         int total = 0;
         for (int i = low; i < high; i++) {
             total += arr[i];
         }
-
         return total;
     }
-    public static int getParallelSum(int[] arr){
+
+    public static int getParallelSum(int[] arr) {
+        //my jvm has 8 processors so Runtime.getRuntime().availableProcessors() will yield 8
         return parallelSum(arr, Runtime.getRuntime().availableProcessors());
     }
+
     public static int parallelSum(int[] arr, int threads) {
         int size = (int) Math.ceil(arr.length * 1.0 / threads);
-
         thread[] sums = new thread[threads];
 
+        //initiating new threads and storing them within the sums array and running them after appending to sums
         for (int i = 0; i < threads; i++) {
             sums[i] = new thread(arr, i * size, (i + 1) * size);
             sums[i].start();
         }
-
+        //when one thread is done with getting sum, the .join starts the next thread and so on
         try {
             for (thread sum : sums) {
                 sum.join();
             }
-        } catch (InterruptedException e) { }
-
+        } catch (InterruptedException ignored) {
+        }
+        //adding together the sum of each thread
         int total = 0;
-
         for (thread sum : sums) {
             total += sum.getPartialSum();
         }
-
         return total;
     }
 
